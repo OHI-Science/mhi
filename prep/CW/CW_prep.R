9d5ea1fd14e4917653439b64e4cc232b342e6618
eschemmel <eschemmel@gmail.com>
2017-06-17 14:16:31 -1000

    spp goal updated with ESA terrestrial plants and animals

---
+++ prep/CW/CW_prep.R
@@ -0,0 +1,21 @@
+#CW prerp
+## setup: libraries, file paths ----
+library(tidyverse) # install.packages('tidyverse')
+dir_layers <- file.path('~/github/mhi/prep/CW')
+
+
+#naming convention of the data file: it is "goalcode_layername_assessmentYEAR.csv".
+data_file  <- file.path(dir_layers, 'cw_full_data.csv')
+d <- readr::read_csv(data_file)
+
+str(d)
+d$sea_water_quality_alert<-as.integer(d$sea_water_quality_alert)
+
+
+library(plyr)#install.packages('plyr')
+d_alerts<-ddply(d, .(rgn_id, Year),summarize,  alerts=sum(sea_water_quality_alert,  na.rm=TRUE))
+
+d_alert<-na.omit(d_alerts)
+dir_layers <- file.path('~/github/mhi/region2017/layers')
+#create the data layer
+readr::write_csv(d_alert, file.path(dir_layers, "cw_alerts_mhi2017.csv"))
