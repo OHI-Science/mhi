f6258614ab75fd7fedb6c9059a5f4099785f7275
jules32 <jstewart@nceas.ucsb.edu>
2017-06-30 15:50:33 -0700

    add flower plot code to calculate_scores; save to figures folder

--- region2017/calculate_scores.r
+++ region2017/calculate_scores.r
@@ -23,3 +23,54 @@ scores = ohicore::CalculateAll(conf, layers)
 write.csv(scores, 'scores.csv', na='', row.names=FALSE)
 
 
+
+## visualize scores ----
+
+
+## source from ohibc until added to ohicore, see https://github.com/OHI-Science/ohibc/blob/master/regionHoweSound/ohibc_howesound_2016.Rmd
+ohibc_url <- 'https://raw.githubusercontent.com/OHI-Science/ohibc/'
+source(paste0(ohibc_url, 'master/src/R/common.R'))
+source(paste0(ohibc_url, 'master/regionHoweSound/plot_flower.R'))
+
+## goal info
+goal_names <- readr::read_csv('conf/goals.csv') %>% select(goal, name)
+weight     <- readr::read_csv('conf/goals.csv') %>% select(goal, weight)
+
+## regions info
+regions <- bind_rows(
+  data_frame(                # order regions to start with whole study_area
+    region_id   = 0,
+    region_name = 'Main Hawaiian Islands'),
+  read_csv('spatial/regions_list.csv') %>%
+    select(region_id   = rgn_id,
+           region_name = rgn_name)) %>%
+  mutate(
+    flower_png = sprintf('figures/flower_%s.png', str_replace_all(region_name, ' ', '_')))
+
+## cycle through each region
+for (i in regions$region_id) { # i = 0
+
+  ## set up for plotting
+  score_df <- scores %>%
+    filter(dimension == 'score') %>%
+    filter(region_id == i) %>%
+    select(-region_id) %>%
+    distinct() %>%
+    inner_join(weight, by = 'goal')
+
+  goal_labels <- score_df %>%
+    left_join(goal_names, by = 'goal') %>%
+    mutate(goal_label = str_replace(name, ' ', '\n'),
+           goal_label = paste(goal_label, round(score), sep = '\n')) %>%
+    select(goal, goal_label)
+
+
+  ## Casey's new flower plot
+  plot_obj <- plot_flower(score_df,
+                          goal_labels = goal_labels,
+                          filename    = regions$flower_png[regions$region_id == i],
+                          incl_legend = TRUE)
+
+}
+
+
