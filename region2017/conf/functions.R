9b7068b7cacc5f9a965171212eeb9d669fc440b0
eschemmel <eschemmel@gmail.com>
2017-08-31 10:08:07 -1000

    removed clean water goal - this will be used as a pressure not an acutal goal based on working group recommendations

--- region2017/conf/functions.R
+++ region2017/conf/functions.R
@@ -1519,63 +1519,6 @@ SP = function(scores){
 }
 
 
-CW = function(layers){
-
-  # layers
-  lyrs <- c('po_pathogens', 'po_nutrients_3nm', 'po_chemicals_3nm', 'po_trash',
-            'cw_chemical_trend', 'cw_nutrient_trend', 'cw_trash_trend', 'cw_pathogen_trend') #global CW layers
-
-  #lyrs <- c('cw_po_lbsp_nosds_bil', 'cw_po_lbsp_sed', 'cw_po_lbspaggolfrunoff', 'cw_po_lbsurbanruoff',  'cw_po_marinedebris', 'cw_po_shipbased_shipp') #local CW layers
-
-
-  d <-  SelectLayersData(layers, layers=lyrs)  %>%
-    select(region_id = id_num, layer, value = val_num)
-
-  ### function to calculate geometric mean:
-  geometric.mean2 <- function (x, na.rm = TRUE) {
-    if (is.null(nrow(x))) {
-      exp(mean(log(x), na.rm = TRUE))
-    }
-    else {
-      exp(apply(log(x), 2, mean, na.rm = na.rm))
-    }
-  }
-
-
-  d_pressures <- d %>%
-    filter(layer %in% grep('po_', lyrs, value=TRUE))  %>%
-    mutate(pressure = 1 - value) %>%  # invert pressures
-    group_by(region_id) %>%
-    dplyr::summarize(score = geometric.mean2(pressure, na.rm=TRUE)) %>% # take geometric mean
-    mutate(score = score * 100) %>%
-    mutate(dimension = "status") %>%
-    ungroup()
-
-  d_trends <- d %>%
-    filter(layer %in% grep('_trend', lyrs, value=TRUE)) %>%
-    mutate(trend = -1 * value)  %>%  # invert trends
-    group_by(region_id) %>%
-    dplyr::summarize(score = mean(trend, na.rm = TRUE)) %>%
-    mutate(dimension = "trend") %>%
-    ungroup()
-
-
-  # return scores
-  scores = rbind(d_pressures, d_trends) %>%
-    mutate(goal = "CW") %>%
-    select(region_id, goal, dimension, score) %>%
-    data.frame()
-  scores$score[scores$goal == 'CW'] = NA
-
-  ## reference points
-  rp <- read.csv(file.path( 'temp/referencePoints.csv'), stringsAsFactors=FALSE) %>%
-    rbind(data.frame(goal = "CW", method = "spatial: pressures scaled from 0-1 at raster level",
-                     reference_point = NA))
-  write.csv(rp, file.path( 'temp/referencePoints.csv'), row.names=FALSE)
-
-  return(scores)
-}
-
 
 HAB = function(layers){
    ## get the data:
