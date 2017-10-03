a3d020f0dc93f3cb835d765d9c5f1f42db6e3075
jules32 <jstewart@nceas.ucsb.edu>
2017-10-03 16:47:50 -0700

    created script for creating conf/weblayers_info/ rmd files

---
+++ region2017/conf/web/create_layers_rmds.R
@@ -0,0 +1,51 @@
+## Create Individual Rmd layers from an EXCEL file for MHI
+
+library(tidyverse)
+library(stringr)
+
+## excel filepath relative to region2017
+dir_excel <- "../prep"
+
+## layers directories locally relative to region2017/conf/web and on github
+dir_layers_confweb <- "conf/web/layers_info/"
+dir_layers_gh <- "https://github.com/OHI-Science/mhi/blob/master/region2017/layers/"
+
+
+## read in excel sheet
+layers_excel <- readxl::read_excel(path = file.path(dir_excel, "OHI_state assessment_framework.xlsx"), sheet = "Data_Layers")
+names(layers_excel)
+
+## rename a few columns
+layers_info <- layers_excel %>%
+  dplyr::select(header_layer = `Data Layer`,
+                layer_name    = Name,
+                description   = `Brief Description`,
+                reference     = Reference) %>%
+  arrange(layer_name)
+
+
+## Loop through all columns and format for web
+
+layers_list <- layers_info$layer_name
+
+# for (i in layers_list) { #
+i = "ao_access"
+
+st <- layers_info %>%
+
+  ## filter for this layer
+  filter(layer_name == i) %>%
+
+  ## all together now
+  mutate(info = paste0("## ", header_layer, "\n\n",
+                       sprintf("[%s](%s)\n\n", layer_name, dir_layers_gh),
+                       description, "\n\n",
+                       "### Reference\n\n",
+                       sprintf("[%s](%s)", reference, "this_will_be_a_link")))
+
+## save rmd
+write_file(st$info, paste0(dir_layers_confweb, st$layer_name, ".Rmd"))
+
+# }
+
+
