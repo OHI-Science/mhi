716b81ff75b4db543fb614847250ff478a7fbfeb
jules32 <jstewart@nceas.ucsb.edu>
2017-11-21 14:36:45 -0800

    create layers page with Eva, and add to nav bar + push

--- _site.R
+++ _site.R
@@ -22,3 +22,28 @@ layers <- readr::read_csv(file.path(dir_scenario_gh, 'layers.csv'))
 weight <- readr::read_csv(file.path(dir_scenario_gh, 'conf/goals.csv')) %>%
   select(goal, weight)
 
+## save local copies of Rmds to knit-child ----
+
+dir_raw_draft <- 'https://raw.githubusercontent.com/OHI-Science/mhi/master'
+
+to_copy <- c('region2017/conf/web/goals.Rmd',
+             'region2017/conf/web/layers_all.Rmd')
+
+for (f in to_copy) { # f <-  'global_supplement/OHI.bib'
+
+  fp <- file.path(dir_raw_draft, f)
+
+  ## if the url exists, save a copy.
+  if (RCurl::url.exists(fp)) {
+    f_web   <- readr::read_lines(fp)
+    if ( tools::file_ext(fp) == 'Rmd' ) {
+      f_local <- paste0('local_', basename(fp))
+    } else {
+      f_local <- basename(fp)
+    }
+    readr::write_lines(f_web, path = f_local, append = FALSE)
+    message(sprintf('saving %s', f_local))
+  } else {
+    message(sprintf('%s does not exist', fp))
+  }
+}
